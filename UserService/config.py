5c61d2e5352fd8f0e40d3ad212b582da370c5ac9
Dhrubojyoti Biswas <46414440+DhrubojyotiBis1@users.noreply.github.com>
2025-11-05 03:37:11 +0530

    Added postgres as main db service (#5)
    Co-authored-by: Dhrubojyoti <dhruv@Dhrubojyotis-MacBook-Air.local>

--- UserService/config.py
+++ UserService/config.py
@@ -4,4 +4,9 @@ class Config:
     GRPC_PORT = "50051"
     JWT_ALGORITHM = "ES256"
     PUBLIC_KEY_PATH = "public_key.pem"
+    USER_DB_USER = "postgres"
+    USER_DB_PASSWORD = "postgres"
+    USER_DB_NAME = "gym_buddy"
+    USER_DB_HOST = "postgres"  
+    USER_DB_PORT = "5432"
     
